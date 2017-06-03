3b9ebc692c315a09bd48c0e6765dc5d8c965d7e2
Coin Graham <cgraham@2ndwatch.com>
2017-06-02 23:06:22 -0400

    updating config.

--- aws_volume_encryption_config.py
+++ aws_volume_encryption_config.py
@@ -2,15 +2,15 @@
 
 # Profile you are referencing for authentication
 # profile = "firstwatch"  # for local testing
-profile = "FSOP_Admin"
+profile = ""
 
 # Region you want to run in.
 # region = "us-east-1"  # for local testing
 region = "us-west-2"
 
 # Comma delimited array of quoted instance names to encrypt the root drive.
-names = ["CLPJDEA5001","CLPJDEA5002","CLPJDEA5003","CLPJDEA5004"]
-# names = ["Test"]
+# names = ["CLPJDEA5005","CLPJDEA5007"]
+names = ["Test"]
 
 # Customer Master Key to use if you're not using the default AWS key.  Full ARN.
-customer_master_key = "arn:aws:kms:us-west-2:644198608768:key/ce34a0b0-f0fd-4fff-b455-d927e28dec60"
+customer_master_key = ""
